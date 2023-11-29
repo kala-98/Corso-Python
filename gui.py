5210023fff2e6a50cf1ff5d2e5b01d822ae5fcac
Ervin Kalabic <ervinita98@gmail.com>
2023-11-29 22:51:39 +0100

    creazione gui

--- gui.py
+++ gui.py
@@ -1,1 +1,9 @@
+# third party library
+import PySimpleGUI as sg
 
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo")
+
+window = sg.Window("My App", layout=[[label, input_box]])
+window.read()
+window.close()
