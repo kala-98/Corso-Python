5ba043a4172f4015dda8e26582692d9db2fe3b8c
Ervin Kalabic <ervinita98@gmail.com>
2023-12-02 12:59:46 +0100

    Compressor app

---
+++ compressor.py
@@ -0,0 +1,24 @@
+# third party library for gui
+import PySimpleGUI as sg
+
+# 1st line
+label1 = sg.Text("Select files to compress: ")
+input1 = sg.Input()
+button1 = sg.FilesBrowse("Choose")
+
+# 2nd line
+label2 = sg.Text("Select destination folder: ")
+input2 = sg.Input()
+button2 = sg.FolderBrowse("Choose")
+
+firstRow = [label1, input1, button1]
+secondRow = [label2, input2, button2]
+
+compressButton = sg.Button("Compress")
+
+window = sg.Window("File Zipper", layout = [firstRow,
+                                            secondRow,
+                                            [compressButton]])
+
+window.read()
+window.close()
--- gui.py
+++ gui.py
@@ -1,9 +1,10 @@
-# third party library
+# third party library for gui
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
 
-window = sg.Window("My App", layout=[[label, input_box]])
+window = sg.Window("My App", layout=[[label, input_box, add_button]])
 window.read()
 window.close()
