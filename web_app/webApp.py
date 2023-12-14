833e0f4699737704843ce3f2a2f1af184a9bbd3f
Ervin Kalabic <ervinita98@gmail.com>
2023-12-14 21:37:50 +0100

    small changes web app

--- web_app/webApp.py
+++ web_app/webApp.py
@@ -2,14 +2,15 @@ import streamlit as st
 import sys
 import os
 
-
-    
-
 # dico a python di considerare anche la cartella padre per recuperare eventuali moduli
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import functions
 
 todos = functions.get_todo()
+
+# "allarga" la finestra
+st.set_page_config(layout="wide")
+
 def add_todo():
     todo = st.session_state["new_todo"] #session_state restituisce una sorta di dizionario
     todos.append(todo + "\n")
@@ -17,7 +18,8 @@ def add_todo():
 
 st.title("Todo App")
 st.subheader("My todo app")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your <b>productivity</b>",
+         unsafe_allow_html=True)
 
 # recupero la lista
 for index, item in enumerate(todos):
