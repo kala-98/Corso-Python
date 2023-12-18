be3f4296f3e8743e2881f3fa9f211e0482c1cf62
Ervin Kalabic <ervinita98@gmail.com>
2023-12-18 21:05:32 +0100

    End of the basics

--- bonus/bonus_json.py
+++ bonus/bonus_json.py
@@ -1,30 +1,30 @@
 
-# import json 
+import json 
 
-# with open("bonus/questions.json", "r") as file:
-#     content = file.read()
+with open("bonus/questions.json", "r") as file:
+    content = file.read()
 
-# data = json.loads(content)
-# count = 0
-# while True:
-#   try:   
-#     for question in data:
-#         # mostro le domande
-#         print(question["question_text"])
-#         # mostro le possibili risposte
-#         for index, answer in enumerate(question["alternatives"], 1):
-#             print(f"{index} - {answer}")
-#         answer = int(input("Enter your answer: "))
-#         print("\n" + "=" * 20 + "\n")
+data = json.loads(content)
+count = 0
+while True:
+  try:   
+    for question in data:
+        # mostro le domande
+        print(question["question_text"])
+        # mostro le possibili risposte
+        for index, answer in enumerate(question["alternatives"], 1):
+            print(f"{index} - {answer}")
+        answer = int(input("Enter your answer: "))
+        print("\n" + "=" * 20 + "\n")
         
-#         if answer == question["correct_answer"]:
-#             count += 1
+        if answer == question["correct_answer"]:
+            count += 1
 
-#     print(f"Your score is {count}/2")
-#     break
+    print(f"Your score is {count}/2")
+    break
 
-#   except ValueError:
-#     print("You have to insert a number!")
-#     continue
+  except ValueError:
+    print("You have to insert a number!")
+    continue
 
 
