8ba5a8d45a571166d4ab350cfc753e0c44fbbab4
Ervin Kalabic <ervinita98@gmail.com>
2023-12-19 22:30:21 +0100

    Developing personal website

--- intermediate/main.py
+++ intermediate/main.py
@@ -1,7 +1,26 @@
-import csv
-with open("intermediate/data.csv", "r") as file:
-    contenuto = csv.reader(file, delimiter = ";")
-    for row in contenuto:
-        print(row)
-    
-        
+import streamlit as st 
+
+st.markdown(
+    """
+    <style>
+    .appview-container {
+    background-color:#808080;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Ervin")
+    content = """
+    Hi i'm a young motivated guy, 
+    eager to learn some skills about 
+    coding in Python
+    """
+    st.write(content)
