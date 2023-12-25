b963996e0bb46b1a201ceeb163fc4a89fb5a7a6f
Ervin Kalabic <ervinita98@gmail.com>
2023-12-25 22:44:02 +0100

    creation mail form

---
+++ intermediate/Individual_Projects/WebSite/pages/Contact_Us.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+#st.set_page_config(layout="wide")
+
+with open("style.css", "r") as file:
+    content_css = file.read()
+
+custom_css = f"""
+    <style>
+    {content_css}
+    </style>
+"""
+
+st.markdown(custom_css, unsafe_allow_html = True)
+
+st.header("Contact Me")
+
+# creazione del form per l'invio della mail (ottimizzato per account gmail)
+with st.form(key = "email_form"):
+    user_email = st.text_input("Your Email address")
+    message = st.text_area("Your Message")
+    button = st.form_submit_button("Submit")
+    if button:
+        message = message + user_email
