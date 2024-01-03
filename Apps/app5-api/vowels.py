a5cc31b80d838c1833c23a4013041c60fb90dd67
Ervin Kalabic <ervinita98@gmail.com>
2024-01-03 16:10:36 +0100

    minor changes

--- Apps/app5-api/vowels.py
+++ Apps/app5-api/vowels.py
@@ -1,24 +1,34 @@
-nome = "Adriano"
-vocali = "aeiou"
-dizionario = {}
 
-for letter in nome.lower():
-    if letter in vocali:
-        if letter in dizionario:
-            dizionario[letter] += 1
-        else:
-            dizionario[letter] = 1
+def calcola_vocali(nome):
+    vocali = "aeiou"
+    booleano = 0
+    dizionario = {}
+    dizionarioOrdinato = {}
+    lista = []
+    somma_vocali = 0
 
-lista = []
-somma_vocali = 0
-for i in dizionario.values():
-    somma_vocali += i
+    for letter in nome.lower():
+        if letter in vocali:
+            booleano = 1
+            if letter in dizionario:
+                dizionario[letter] += 1
+            else:
+                dizionario[letter] = 1
+    
+    if booleano == 1:
+        for i in dizionario.values():
+            somma_vocali += i
 
-for x, y in dizionario.items():
-    lista.append(f"{x} - {y}")
+        dizionarioOrdinato = sorted(dizionario.items(), key=lambda x: x[1], reverse=True)
+    
+        for x, y in dizionarioOrdinato:
+            lista.append(f"{x} - {y}")
 
+        print(f"Il nome {nome} contiene al suo interno {somma_vocali} {'vocali totali, distribuite in questo modo:' if somma_vocali > 1 else 'vocale:'}")
 
-print(f"Il nome {nome} contiene al suo interno {somma_vocali} vocali totali, distribuite in questo modo:")
+        for index, elemento in enumerate (lista, 1):
+            print(f"{index}: {elemento}")
+    else:
+        print("Il nome che hai inserito non presenta vocali")
 
-for index, elemento in enumerate (lista, 1):
-    print(f"{index}: {elemento}")
+calcola_vocali("Giaa")
