0d87837743a5f071912fe694ecd33de9a8838172
Ervin Kalabic <ervinita98@gmail.com>
2024-01-03 22:17:43 +0100

    creating the dictionary api template

--- Apps/app6-weather-api/main2.py
+++ Apps/app6-weather-api/main2.py
@@ -12,16 +12,14 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home2.html")
 
-@app.route("/api/v1/<station>/<date>")
-def api(station, date):
-    df = pd.read_csv()
-    return {"station": station,
-            "date": date,
-            "temperature": temperature}
+@app.route("/api/v1/<word>")
+def api(word):
+    return {"definition": word.upper(),
+            "word": word}
 
 # the app has to enabled only when we run this script directly
 # in order to run multiple apps, we can specify the port number
 if __name__ == "__main__":
-   app.run(debug = True, port = 5001)
+   app.run(debug = True, port = 5000)
