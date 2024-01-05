fb091b412a916803c3b91e11abfe0e0fd870d7f0
Ervin Kalabic <ervinita98@gmail.com>
2024-01-05 17:30:02 +0100

    return temperature for any station

--- Apps/app6-weather-api/functions.py
+++ Apps/app6-weather-api/functions.py
@@ -4,27 +4,33 @@ import numpy as np
 
 PATH = "data_small/"
 def read_file_csv(station, date, filepath = PATH):
+    file_tg = ""
+    #lista  = os.listdir(PATH)
+    #fileName = [file for file in lista if file.endswith(f"{str(station)}.txt")]
+    len_station = len(str(station))
 
-    lista = os.listdir(PATH)
-    #lista_file = [file for file in lista if file.startswith("TG_")]
-    fileName = [file for file in lista if file.endswith(f"{str(station)}.txt")]
+    # recupero il nome corretto del file
+    if len_station < 6:
+        file_tg = "0" * (6 - len_station) + str(station)
+    else:
+        file_tg = str(station)
+    fileName = f"TG_STAID{file_tg}.txt"   
 
-    for file_tg in fileName:
-        with open(PATH + file_tg, "r") as file:
-            df = pd.read_csv(file, skiprows=20, parse_dates = ["    DATE"])
+    with open(PATH + fileName, "r") as file:
+        df = pd.read_csv(file, skiprows=20, parse_dates = ["    DATE"])
 
-            # il metodo mask esclude tutti quelli con valore -9999 nella colonna "    TG" e gli sostituisce con nan
-            df["TG0"] =  df["   TG"].mask(df["   TG"] == -9999, np.nan)
-            # normalizzo i dati
-            df["TG"] = df["TG0"] / 10
+        # il metodo mask esclude tutti quelli con valore -9999 nella colonna "    TG" e gli sostituisce con nan
+        df["TG0"] =  df["   TG"].mask(df["   TG"] == -9999, np.nan)
+        # normalizzo i dati
+        df["TG"] = df["TG0"] / 10
 
-            result = df.loc[df["    DATE"] == date]["TG"]
-            if result.dropna().empty:
-                result = None  
-                return "There are no information available for that date, sorry"
-            else:
-                result = result.squeeze()
-                return result
+        result = df.loc[df["    DATE"] == date]["TG"]
+        if result.dropna().empty:
+            result = None  
+            return "There are no information available for that date, sorry"
+        else:
+            result = result.squeeze()
+            return result
             
 if __name__ == "__main__":
     print("Ok")
