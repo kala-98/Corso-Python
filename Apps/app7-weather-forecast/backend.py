96e707ff7c46e839b4fb19ba628e0fa264a90379
ervinita98@gmail.com <ervinita98@gmail.com>
2024-01-11 22:33:17 +0100

    finishing the weather forecast's app

--- Apps/app7-weather-forecast/backend.py
+++ Apps/app7-weather-forecast/backend.py
@@ -2,22 +2,18 @@ import requests
 
 API_KEY = '85d4473c86d7a3f25058ce9afe0cf0a3'
 # city_ID = "London"
-# url = f"http://api.openweathermap.org/data/2.5/forecast?id={city_ID}&appid={api_key}"
+# url = f"http://api.openweathermap.org/data/2.5/forecast?q=London&appid={API_KEY}"
 # response = requests.get(url)
 # print(response.json())
 
-def get_data(place, forecast_days = 2, kind = "Sky"):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+def get_data(place, forecast_days):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place = "Tokyo"))
+    print(get_data(place = "Tokyo", forecast_days = 3))
--- Apps/app7-weather-forecast/main.py
+++ Apps/app7-weather-forecast/main.py
@@ -1,24 +1,38 @@
 import streamlit as st 
 import plotly.express as px
 from backend import get_data
+try:
+    # front-end's creation
+    st.title("Weather Forecast for the Next Days")
+    place = st.text_input("Place: ")
 
-# front-end's creation
-st.title("Weather Forecast for the Next Days")
-place = st.text_input("Place: ")
-# place = st.selectbox("Place: ", options = [])
-days = st.slider("Forecasted Days: ", min_value = 1, max_value = 5,
-                 help = "Select the number of forecasted days") # with help they can see more information about the widget
-option = st.selectbox("Select data to view", ("Temperature", "Sky"))
+    days = st.slider("Forecasted Days: ", min_value = 1, max_value = 5,
+                    help = "Select the number of forecasted days") # with help they can see more information about the widget
+    option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
-if place != "":
-    st.subheader(f"{option} for the next {str(days) + " day in " if days == 1 else str(days) + " days in "} {place}")
+    if place:
 
-# making dynamic plot 
-# requires a "fig" (we create it through data visualization library such as plotly)
-    
-data = get_data(place, days, option)
 
-# dates, temperatures
+        # get the temperature/sky data
+        filtered_data = get_data(place, days)
 
-fig = px.line(x = dates[0:days], y = temperatures[0:days], labels= {"x": "Date", "y": "Temperatures (°C)"})
-st.plotly_chart(fig)
+        # making dynamic plot 
+        #requires a "fig" (we create it through data visualization library such as plotly)
+
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            fig = px.line(x = dates, y = temperatures, labels= {"x": "Date", "y": "Temperatures (°C)"})
+            st.subheader(f"{option} for the next {str(days) + " day in " if days == 1 else str(days) + " days in "} {place}")
+            st.plotly_chart(fig)
+
+        if option == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain":"images/rain.png", "Snow":"images/snow.png"}
+            image_path = [images[condition] for condition in sky_conditions]
+            st.subheader(f"{option} for the next {str(days) + " day in " if days == 1 else str(days) + " days in "} {place}")
+            st.image(image_path, width = 115)
+
+except:
+    st.write('<span style="color:red; font-size:30px">Please insert a valid city </span>', unsafe_allow_html=True)
+
