434d38f2224d3791a59f79e7e53815f2af155bec
ervinita98@gmail.com <ervinita98@gmail.com>
2024-01-16 23:12:28 +0100

    creating a diary to analyze the mood

--- Apps/app8-processing-books/diary.py
+++ Apps/app8-processing-books/diary.py
@@ -1,1 +1,32 @@
 import streamlit as st
+import plotly.express as px
+import glob 
+from nltk.sentiment import SentimentIntensityAnalyzer
+from datetime import datetime
+
+lista_filePath = glob.glob("diary/*.txt")
+
+list_pos = []
+list_neg = []
+
+dates = [name.strip(".txt").strip("diary\\") for name in lista_filePath]
+
+analyzer = SentimentIntensityAnalyzer()
+
+for filePath in lista_filePath:
+    with open(filePath, "r") as file:
+        content = file.read()
+        scores = analyzer.polarity_scores(content)
+        list_pos.append(scores["pos"])
+        list_neg.append(scores["neg"])
+
+st.title("Diary Tone")
+st.subheader("Positivity")
+
+fig = px.line(x = dates, y = list_pos, labels = {"x": "Date", "y": "Positivity"})
+st.plotly_chart(fig)
+
+st.subheader("Negativity")
+fig = px.line(x = dates, y = list_neg, labels = {"x": "Date", "y": "Negativity"})
+st.plotly_chart(fig)
+
