7827d7e4e0954ba35d52aae40cf938ba1e6df5e3
ervinita98@gmail.com <ervinita98@gmail.com>
2024-02-22 23:09:13 +0100

    ending the django setting's project

--- Apps/app13-jobapplication/django/job_application/models.py
+++ Apps/app13-jobapplication/django/job_application/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# defining the database's model
+class Form(models.Model): 
+    first_name = models.CharField(max_length = 80)
+    last_name = models.CharField(max_length = 80)
+    email = models.EmailField()
+    date = models.DateField()
+    occupation = models.CharField(max_length = 80)
+
+    # obtain a string representation of the instance
+    # (what should be printed when the print is used)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
